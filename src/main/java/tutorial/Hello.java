c5779709f08b87264d070fbd27f4524f261ed10e
Khin Chan Myae Thu <khinchanmyaethu.kcmt2000@gmail.com>
2023-06-09 08:56:19 +0630

    Welcome file

---
+++ src/main/java/tutorial/Hello.java
@@ -0,0 +1,7 @@
+package main.java.tutorial;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author khinchanmyaethu
+ *
+ */
+module ojtTutorial {
+}
