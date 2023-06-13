2d2e813c787d31b5def7f96839e26ff5f5488385
Khin Chan Myae Thu <khinchanmyaethu.kcmt2000@gmail.com>
2023-06-13 12:09:11 +0630

    Finished tutorial6

--- src/main/java/tutorial/Tutorial6.java
+++ src/main/java/tutorial/Tutorial6.java
@@ -1,47 +1,29 @@
 package main.java.tutorial;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class Tutorial6 {
+		public static void main(String[] args) {
+	    	Scanner scanner = new Scanner(System.in);
+	    	System.out.print("Enter your password: ");
+	    	String password = scanner.nextLine();
+	    	
+	        if (isStrongPassword(password)) {
+	            System.out.println("The password is strong.");
+	        } else {
+	            System.out.println("The password is weak");
+	        }
+	        scanner.close();
+	    }
 
-	public static void Strongpw(String input)
-    {
-        int n = input.length();
-        boolean lowerChar = false,
-        		upperChar = false,
-                digit = false,
-                specialChar = false;
-        Set<Character> set = new HashSet<Character>(
-            Arrays.asList('!', '@', '#', '$', '%', '^', '&',
-                          '*', '(', ')', '-', '+'));
-        for (char i : input.toCharArray())
-        {
-            if (Character.isLowerCase(i))
-            	lowerChar = true;
-            if (Character.isUpperCase(i))
-            	upperChar = true;
-            if (Character.isDigit(i))
-                digit = true;
-            if (set.contains(i))
-                specialChar = true;
-        }
-       
-        // Strength of password
-        System.out.print("Strength of password:- ");
-        if (digit && lowerChar && upperChar && specialChar && (n >= 8))
-            System.out.print(" Strong");
-        else if ((lowerChar || upperChar || specialChar) && (n >= 6))
-            System.out.print(" Moderate");
-        else
-            System.out.print(" Weak");
-    }
- 
-    public static void main(String[] args)
-    {
-        //String input = "Apple@135";
-    	Scanner sc = new Scanner(System.in);
-    	System.out.println("Enter password :");
-    	String input = sc.nextLine();
-        Strongpw(input);
-        sc.close();
-    }
- 
+	    public static boolean isStrongPassword(String password) {
+	        String pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\\\d)(?=.*[@#$%^&+=]).{8,}$";
+
+	 
+	        Pattern regex = Pattern.compile(pattern);
+
+	        Matcher matcher = regex.matcher(password);
+	        
+	        return matcher.matches();
+	    }
 }
